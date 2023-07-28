eae7675cf7aa6e98c114f62956144c4e28ab9ca9
mannyjv <emmanuel.vazquez06@gmail.com>
2023-07-28 12:51:26 +0200

    feature: hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+
+import Exercises from "./Exercises";
+import SearchExercises from "./SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-  return <div>Home</div>;
+  // const [data, setData] = useState(second);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
 
 export default Home;
